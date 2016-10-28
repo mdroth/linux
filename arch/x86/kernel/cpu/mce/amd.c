993a14f5444ec48bb096e6fe1c7dcdba8abaf1ba
Yazen Ghannam <Yazen.Ghannam@amd.com>
2016-10-28 15:18:00 -0500

    [INTERNAL ONLY] x86/mce/AMD: Set default threshold limit to 1 for bringup
    This will cause the correctable threshold interrupt handler to trigger for
    every correctable error, so we don't miss any.
    
    Signed-off-by: Yazen Ghannam <Yazen.Ghannam@amd.com>

--- arch/x86/kernel/cpu/mce/amd.c
+++ arch/x86/kernel/cpu/mce/amd.c
@@ -438,7 +438,7 @@ static void mce_threshold_block_init(struct threshold_block *b, int offset)
 		.lvt_off		= offset,
 	};
 
-	b->threshold_limit		= THRESHOLD_MAX;
+	b->threshold_limit		= 1;
 	threshold_restart_bank(&tr);
 };
 
@@ -1262,7 +1262,7 @@ static int allocate_threshold_blocks(unsigned int cpu, struct threshold_bank *tb
 	b->address		= address;
 	b->interrupt_enable	= 0;
 	b->interrupt_capable	= lvt_interrupt_supported(bank, high);
-	b->threshold_limit	= THRESHOLD_MAX;
+	b->threshold_limit	= 1;
 
 	if (b->interrupt_capable) {
 		threshold_ktype.default_attrs[2] = &interrupt_enable.attr;
