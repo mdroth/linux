1df957954daba3603015aaee3ee91baf95e9b440
Nathan Fontenot <nathan.fontenot@amd.com>
2020-06-16 08:11:28 -0500

    [INTERNAL] drivers/misc: Provide page fault hooks for hardware tracing.
    To assist with hardware debugging so that their buffers are not filled
    during the handling of a page fault, this patch provides hooks on the
    entry and exit of the page fault handler to inform hardware that we
    are entering/exiting the page fault handler. This will allow hardware
    to selectively disable their logging mechanisms while the page fault
    handler runs.
    
    To control this behavior a sysfs file is created,
    /sys/kernel/mm/hw_pf_tracing, to provide controls to enable the
    notifications and select which SMT thread to notify on.
    
    Additionally, there is a sysfs file (/sys/kernel/mm/hw_pf_stats)
    that will display per-cpu page fault hits while this is enabled.
    
    Signed-off-by: Nathan Fontenot <nathan.fontenot@amd.com>

--- arch/x86/mm/fault.c
+++ arch/x86/mm/fault.c
@@ -34,6 +34,8 @@
 #define CREATE_TRACE_POINTS
 #include <asm/trace/exceptions.h>
 
+#include "amd_hw_pf_trace.c"
+
 /*
  * Returns 0 if mmiotrace is disabled, or if the fault is not
  * handled by mmiotrace:
@@ -760,6 +762,8 @@ no_context(struct pt_regs *regs, unsigned long error_code,
 		 * faulting through the emulate_vsyscall() logic.
 		 */
 		if (current->thread.sig_on_uaccess_err && signal) {
+			hw_pf_tracing_exit(-1);
+
 			set_signal_archinfo(address, error_code);
 
 			/* XXX: hwpoison faults will set the wrong code. */
@@ -859,6 +863,8 @@ show_signal_msg(struct pt_regs *regs, unsigned long error_code,
 {
 	const char *loglvl = task_pid_nr(tsk) > 1 ? KERN_INFO : KERN_EMERG;
 
+	hw_pf_tracing_exit(-1);
+
 	if (!unhandled_signal(tsk, SIGSEGV))
 		return;
 
@@ -913,6 +919,8 @@ __bad_area_nosemaphore(struct pt_regs *regs, unsigned long error_code,
 		 * layout, pretend that user-mode accesses to kernel addresses
 		 * are always protection faults.
 		 */
+		hw_pf_tracing_exit(-1);
+
 		if (address >= TASK_SIZE_MAX)
 			error_code |= X86_PF_PROT;
 
@@ -1300,6 +1308,7 @@ do_kern_addr_fault(struct pt_regs *regs, unsigned long hw_error_code,
 }
 NOKPROBE_SYMBOL(do_kern_addr_fault);
 
+
 /* Handle faults in the user portion of the address space */
 static inline
 void do_user_addr_fault(struct pt_regs *regs,
@@ -1315,16 +1324,22 @@ void do_user_addr_fault(struct pt_regs *regs,
 	tsk = current;
 	mm = tsk->mm;
 
+	hw_pf_tracing_enter();
+
 	/* kprobes don't want to hook the spurious faults: */
-	if (unlikely(kprobe_page_fault(regs, X86_TRAP_PF)))
+	if (unlikely(kprobe_page_fault(regs, X86_TRAP_PF))) {
+		hw_pf_tracing_exit(0);
 		return;
+	}
 
 	/*
 	 * Reserved bits are never expected to be set on
 	 * entries in the user portion of the page tables.
 	 */
-	if (unlikely(hw_error_code & X86_PF_RSVD))
-		pgtable_bad(regs, hw_error_code, address);
+	if (unlikely(hw_error_code & X86_PF_RSVD)) {
+		hw_pf_tracing_exit(-1);
+		pgtable_bad(regs, hw_error_code, address); /* fail */
+	}
 
 	/*
 	 * If SMAP is on, check for invalid kernel (supervisor) access to user
@@ -1338,6 +1353,7 @@ void do_user_addr_fault(struct pt_regs *regs,
 		     !(regs->flags & X86_EFLAGS_AC)))
 	{
 		bad_area_nosemaphore(regs, hw_error_code, address);
+		hw_pf_tracing_exit(0);
 		return;
 	}
 
@@ -1347,6 +1363,7 @@ void do_user_addr_fault(struct pt_regs *regs,
 	 */
 	if (unlikely(faulthandler_disabled() || !mm)) {
 		bad_area_nosemaphore(regs, hw_error_code, address);
+		hw_pf_tracing_exit(0);
 		return;
 	}
 
@@ -1386,7 +1403,7 @@ void do_user_addr_fault(struct pt_regs *regs,
 	 */
 	if (is_vsyscall_vaddr(address)) {
 		if (emulate_vsyscall(hw_error_code, regs, address))
-			return;
+			return; /* ??? */
 	}
 #endif
 
@@ -1409,7 +1426,8 @@ void do_user_addr_fault(struct pt_regs *regs,
 			 * which we do not expect faults.
 			 */
 			bad_area_nosemaphore(regs, hw_error_code, address);
-			return;
+			hw_pf_tracing_exit(0);
+			return; /* failure */
 		}
 retry:
 		down_read(&mm->mmap_sem);
@@ -1425,16 +1443,19 @@ retry:
 	vma = find_vma(mm, address);
 	if (unlikely(!vma)) {
 		bad_area(regs, hw_error_code, address);
+		hw_pf_tracing_exit(0);
 		return;
 	}
 	if (likely(vma->vm_start <= address))
 		goto good_area;
 	if (unlikely(!(vma->vm_flags & VM_GROWSDOWN))) {
 		bad_area(regs, hw_error_code, address);
+		hw_pf_tracing_exit(0);
 		return;
 	}
 	if (unlikely(expand_stack(vma, address))) {
 		bad_area(regs, hw_error_code, address);
+		hw_pf_tracing_exit(0);
 		return;
 	}
 
@@ -1445,6 +1466,7 @@ retry:
 good_area:
 	if (unlikely(access_error(hw_error_code, vma))) {
 		bad_area_access_error(regs, hw_error_code, address, vma);
+		hw_pf_tracing_exit(0);
 		return;
 	}
 
@@ -1466,9 +1488,14 @@ good_area:
 
 	/* Quick path to respond to signals */
 	if (fault_signal_pending(fault, regs)) {
-		if (!user_mode(regs))
+		if (!user_mode(regs)) {
 			no_context(regs, hw_error_code, address, SIGBUS,
 				   BUS_ADRERR);
+			hw_pf_tracing_exit(0);
+		} else {
+			hw_pf_tracing_exit(-1);
+		}
+
 		return;
 	}
 
@@ -1486,6 +1513,7 @@ good_area:
 	up_read(&mm->mmap_sem);
 	if (unlikely(fault & VM_FAULT_ERROR)) {
 		mm_fault_error(regs, hw_error_code, address, fault);
+		hw_pf_tracing_exit(0);
 		return;
 	}
 
@@ -1502,6 +1530,7 @@ good_area:
 	}
 
 	check_v8086_mode(regs, address, tsk);
+	hw_pf_tracing_exit(0);
 }
 NOKPROBE_SYMBOL(do_user_addr_fault);
 
