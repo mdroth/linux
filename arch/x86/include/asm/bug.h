7ccf40f89bcab832dd2bec761abccd870d693c8e
Kim Phillips <kim.phillips@amd.com>
2020-08-14 17:17:58 -0500

    [INTERNAL ONLY] Early warning for BUG/WARN executing a UD2 instruction
    The Cores team need to know whether a UD2 they see too late came
    from one of the kernel's BUG or WARN assertions.
    
    If warn_ud2 is set on the kernel command line, any BUG or WARN will
    issue a warning and dump the stack on dmesg for serial capture
    prior to continuing to execute the ud2 instruction.
    
    At boot, a console message will always say whether this UD2 warning
    feature is enabled.  It is off by default.
    
    Signed-off-by: Kim Phillips <kim.phillips@amd.com>

--- arch/x86/include/asm/bug.h
+++ arch/x86/include/asm/bug.h
@@ -68,10 +68,18 @@ do {									\
 
 #endif /* CONFIG_GENERIC_BUG */
 
+#include <linux/kernel.h>
+
+extern bool warn_ud2;
+
 #define HAVE_ARCH_BUG
 #define BUG()							\
 do {								\
 	instrumentation_begin();				\
+	if (warn_ud2) {						\
+		printk("%s %s %d: in BUG: dumping stack, then UD2\n", __FILE__, __func__, __LINE__);	\
+		dump_stack();					\
+	}							\
 	_BUG_FLAGS(ASM_UD2, 0);					\
 	unreachable();						\
 } while (0)
@@ -85,6 +93,10 @@ do {								\
 #define __WARN_FLAGS(flags)					\
 do {								\
 	instrumentation_begin();				\
+	if (warn_ud2) {						\
+		printk("%s %s %d: in WARN: dumping stack, then UD2\n", __FILE__, __func__, __LINE__);	\
+		dump_stack();					\
+	}							\
 	_BUG_FLAGS(ASM_UD2, BUGFLAG_WARNING|(flags));		\
 	annotate_reachable();					\
 	instrumentation_end();					\
