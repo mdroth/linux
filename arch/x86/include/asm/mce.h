b730380a83c6eed24674148097db2b6cd2654844
Muralidhara M K <muralimk@amd.com>
2021-04-20 17:05:10 -0400

    [WIP] x86/MCE/AMD, EDAC/mce_amd: Add new SMCA bank types.
    Add the (HWID, MCATYPE) tuples and names for new
    SMCA bank types.
    
    Also, add their respective error descriptions to the MCE
    decoding module edac_mce_amd.
    
    Signed-off-by: Muralidhara M K <muralimk@amd.com>

--- arch/x86/include/asm/mce.h
+++ arch/x86/include/asm/mce.h
@@ -339,6 +339,7 @@ enum smca_bank_types {
 	SMCA_CS_V2,	/* Coherent Slave */
 	SMCA_PIE,	/* Power, Interrupts, etc. */
 	SMCA_UMC,	/* Unified Memory Controller */
+	SMCA_UMC_V2,	/* Unified Memory Controller */
 	SMCA_PB,	/* Parameter Block */
 	SMCA_PSP,	/* Platform Security Processor */
 	SMCA_PSP_V2,	/* Platform Security Processor */
@@ -347,6 +348,10 @@ enum smca_bank_types {
 	SMCA_MP5,	/* Microprocessor 5 Unit */
 	SMCA_NBIO,	/* Northbridge IO Unit */
 	SMCA_PCIE,	/* PCI Express Unit */
+	SMCA_PCIE_V2,	/* PCI Express Unit */
+	SMCA_XGMI_PCS,	/* xGMI PCS Unit */
+	SMCA_XGMI_PHY,	/* xGMI PHY Unit */
+	SMCA_WAFL_PHY,	/* WAFL PHY Unit */
 	N_SMCA_BANK_TYPES
 };
 
