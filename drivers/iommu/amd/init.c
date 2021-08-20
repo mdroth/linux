09d5d60be3a84f0363f0df962012fa022214a62e
Wei Huang <wei.huang2@amd.com>
2021-08-20 15:29:56 -0500

    iommu/amd: Relocate GAMSup check to early_enable_iommus
    Currently, iommu_init_ga() checks and disables IOMMU VAPIC support
    (i.e. AMD AVIC support in IOMMU) when GAMSup feature bit is not set.
    However it forgets to clear IRQ_POSTING_CAP from the previously set
    amd_iommu_irq_ops.capability.
    
    This triggers an invalid page fault bug during guest VM warm reboot
    if AVIC is enabled since the irq_remapping_cap(IRQ_POSTING_CAP) is
    incorrectly set, and crash the system with the following kernel trace.
    
        BUG: unable to handle page fault for address: 0000000000400dd8
        RIP: 0010:amd_iommu_deactivate_guest_mode+0x19/0xbc
        Call Trace:
         svm_set_pi_irte_mode+0x8a/0xc0 [kvm_amd]
         ? kvm_make_all_cpus_request_except+0x50/0x70 [kvm]
         kvm_request_apicv_update+0x10c/0x150 [kvm]
         svm_toggle_avic_for_irq_window+0x52/0x90 [kvm_amd]
         svm_enable_irq_window+0x26/0xa0 [kvm_amd]
         vcpu_enter_guest+0xbbe/0x1560 [kvm]
         ? avic_vcpu_load+0xd5/0x120 [kvm_amd]
         ? kvm_arch_vcpu_load+0x76/0x240 [kvm]
         ? svm_get_segment_base+0xa/0x10 [kvm_amd]
         kvm_arch_vcpu_ioctl_run+0x103/0x590 [kvm]
         kvm_vcpu_ioctl+0x22a/0x5d0 [kvm]
         __x64_sys_ioctl+0x84/0xc0
         do_syscall_64+0x33/0x40
         entry_SYSCALL_64_after_hwframe+0x44/0xae
    
    Fixes by moving the initializing of AMD IOMMU interrupt remapping mode
    (amd_iommu_guest_ir) earlier before setting up the
    amd_iommu_irq_ops.capability with appropriate IRQ_POSTING_CAP flag.
    
    Signed-off-by: Wei Huang <wei.huang2@amd.com>
    Signed-off-by: Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>

--- drivers/iommu/amd/init.c
+++ drivers/iommu/amd/init.c
@@ -866,13 +866,6 @@ static int iommu_init_ga(struct amd_iommu *iommu)
 	int ret = 0;
 
 #ifdef CONFIG_IRQ_REMAP
-	/* Note: We have already checked GASup from IVRS table.
-	 *       Now, we need to make sure that GAMSup is set.
-	 */
-	if (AMD_IOMMU_GUEST_IR_VAPIC(amd_iommu_guest_ir) &&
-	    !iommu_feature(iommu, FEATURE_GAM_VAPIC))
-		amd_iommu_guest_ir = AMD_IOMMU_GUEST_IR_LEGACY_GA;
-
 	ret = iommu_init_ga_log(iommu);
 #endif /* CONFIG_IRQ_REMAP */
 
@@ -2489,6 +2482,14 @@ static void early_enable_iommus(void)
 	}
 
 #ifdef CONFIG_IRQ_REMAP
+	/*
+	 * Note: We have already checked GASup from IVRS table.
+	 *       Now, we need to make sure that GAMSup is set.
+	 */
+	if (AMD_IOMMU_GUEST_IR_VAPIC(amd_iommu_guest_ir) &&
+	    !check_feature_on_all_iommus(FEATURE_GAM_VAPIC))
+		amd_iommu_guest_ir = AMD_IOMMU_GUEST_IR_LEGACY_GA;
+
 	if (AMD_IOMMU_GUEST_IR_VAPIC(amd_iommu_guest_ir))
 		amd_iommu_irq_ops.capability |= (1 << IRQ_POSTING_CAP);
 #endif
