6b7089ffba11ccb80ddb56598a2f6bd806e5c2ce
Kim Phillips <kim.phillips@amd.com>
2021-10-25 15:36:32 -0500

    [WIP] x86/cpufeatures: Add support for cpuid leaf 80000021/EAX (FeatureExt2Eax)
    Future AMD processors advertise new features in this leaf.
    Add the new leaf and its Automatic IBRS feature bit.
    
    Note: New whole leaf (vs a bit) due to propagation via KVM
    later in this series.
    
    Signed-off-by: Kim Phillips <kim.phillips@amd.com>
    Cc: Borislav Petkov <bp@suse.de>
    Cc: Brijesh Singh <brijesh.singh@amd.com>
    Cc: David Woodhouse <dwmw@amazon.co.uk>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Dave Hansen <dave.hansen@linux.intel.com>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Cc: Juergen Gross <jgross@suse.com>
    Cc: Sean Christopherson <seanjc@google.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Tony Luck <tony.luck@intel.com>
    Cc: Paolo Bonzini <pbonzini@redhat.com>
    Cc: x86@kernel.org
    Cc: linux-kernel@vger.kernel.org

--- arch/x86/include/asm/cpufeatures.h
+++ arch/x86/include/asm/cpufeatures.h
@@ -13,7 +13,7 @@
 /*
  * Defines x86 CPU feature bits
  */
-#define NCAPINTS			20	   /* N 32-bit words worth of info */
+#define NCAPINTS			21	   /* N 32-bit words worth of info */
 #define NBUGINTS			1	   /* N 32-bit bug flags */
 
 /*
@@ -405,6 +405,9 @@
 #define X86_FEATURE_VM_PAGE_FLUSH	(19*32+ 2) /* "" VM Page Flush MSR is supported */
 #define X86_FEATURE_SEV_ES		(19*32+ 3) /* AMD Secure Encrypted Virtualization - Encrypted State */
 #define X86_FEATURE_SME_COHERENT	(19*32+10) /* "" AMD hardware-enforced cache coherency */
+
+/* AMD-defined Extended Feature 2 EAX, CPUID level 0x80000021 (EAX), word 20 */
+#define X86_FEATURE_AUTOIBRS		(20*32+ 8) /* AMD Automatic IBRS */
 
 /*
  * BUG word(s)
