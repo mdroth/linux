1a9d03a36a27d9e1c49921bdf0e6716a59f6c267
Michael Roth <michael.roth@amd.com>
2021-12-07 15:46:25 -0600

    kvm: s390: selftests: use ucall_ops to define default ucall implementation
    As with x86, switch the default ucall implementation to using the new
    ucall_ops infrastructure. With this change ucall_init/ucall_uninit are
    no longer arch-specific and can now be dropped in favor of the ones in
    ucall_common.c.
    
    Signed-off-by: Michael Roth <michael.roth@amd.com>

--- tools/testing/selftests/kvm/include/kvm_util.h
+++ tools/testing/selftests/kvm/include/kvm_util.h
@@ -8,16 +8,6 @@
 #define SELFTEST_KVM_UTIL_H
 
 #include "kvm_util_base.h"
-/*
- * TODO: ucall.h contains arch-specific declarations along with
- * ucall_common.h. For now only a subset of archs provide the
- * new header. Once all archs implement the new header the #include for
- * ucall_common.h can be dropped.
- */
-#if defined(__x86_64__) || defined (__aarch64__)
 #include "ucall.h"
-#else
-#include "ucall_common.h"
-#endif
 
 #endif /* SELFTEST_KVM_UTIL_H */
