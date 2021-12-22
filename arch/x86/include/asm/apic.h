65233ffd5f375ed7841151c1ae125ba569d8bd5a
root <root@spgblr-dyt-04.amd.com>
2021-12-22 18:46:59 -0500

    Revert "[IN REVIEW] x86/apic: Add helper function to get maximum physical APIC ID"
    This reverts commit efe89daeb4f6f104661ac837a14cbd45e331c0b2.

--- arch/x86/include/asm/apic.h
+++ arch/x86/include/asm/apic.h
@@ -435,7 +435,6 @@ static inline void apic_set_eoi_write(void (*eoi_write)(u32 reg, u32 v)) {}
 #endif /* CONFIG_X86_LOCAL_APIC */
 
 extern void apic_ack_irq(struct irq_data *data);
-extern u32 apic_get_max_phys_apicid(void);
 
 static inline void ack_APIC_irq(void)
 {
