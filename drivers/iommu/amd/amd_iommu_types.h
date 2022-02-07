5517b7038b4896ab5d59b16947805da2b827a611
Vasant Hegde <vasant.hegde@amd.com>
2022-02-07 15:57:24 +0530

    [WIP] iommu/amd: Remove global amd_iommu_last_bdf
    Replace it with per PCI segment last_bdf variable.
    
    Signed-off-by: Vasant Hegde <vasant.hegde@amd.com>

--- drivers/iommu/amd/amd_iommu_types.h
+++ drivers/iommu/amd/amd_iommu_types.h
@@ -830,9 +830,6 @@ struct unity_map_entry {
 /* size of the dma_ops aperture as power of 2 */
 extern unsigned amd_iommu_aperture_order;
 
-/* largest PCI device id we expect translation requests for */
-extern u16 amd_iommu_last_bdf;
-
 /* allocation bitmap for domain ids */
 extern unsigned long *amd_iommu_pd_alloc_bitmap;
 
