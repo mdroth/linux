713a6c90dabadfd70e14dbceb60cd70f46617902
Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
2022-02-14 09:11:32 -0600

    Revert "[WIP] iommu/amd: Introduce get_device_sbdf_id() helper function"
    This reverts commit 09e7deeb256db8ac390bb1ebcebbed29078a8f1e.

--- drivers/iommu/amd/iommu.c
+++ drivers/iommu/amd/iommu.c
@@ -92,13 +92,11 @@ static void detach_device(struct device *dev);
  *
  ****************************************************************************/
 
-static inline int get_pci_device_id(struct device *dev)
+static inline u16 get_pci_device_id(struct device *dev)
 {
 	struct pci_dev *pdev = to_pci_dev(dev);
-	int seg = pci_domain_nr(pdev->bus);
-	u16 devid = pci_dev_id(pdev);
 
-	return ((seg << 16) | (devid & 0xffff));
+	return pci_dev_id(pdev);
 }
 
 static inline int get_acpihid_device_id(struct device *dev,
@@ -121,7 +119,7 @@ static inline int get_acpihid_device_id(struct device *dev,
 	return -EINVAL;
 }
 
-static inline int get_device_sbdf_id(struct device *dev)
+static inline int get_device_id(struct device *dev)
 {
 	int devid;
 
@@ -184,11 +182,9 @@ static struct amd_iommu *__rlookup_amd_iommu(u16 seg, u16 devid)
 static struct amd_iommu *rlookup_amd_iommu(struct device *dev)
 {
 	u16 seg = get_device_segment(dev);
-	int devid = get_device_sbdf_id(dev);
+	u16 devid = get_device_id(dev);
 
-	if (devid < 0)
-		return false;
-	return __rlookup_amd_iommu(seg, (devid & 0xffff));
+	return __rlookup_amd_iommu(seg, devid);
 }
 
 static struct protection_domain *to_pdomain(struct iommu_domain *dom)
@@ -368,10 +364,9 @@ static bool check_device(struct device *dev)
 	if (!dev)
 		return false;
 
-	devid = get_device_sbdf_id(dev);
+	devid = get_device_id(dev);
 	if (devid < 0)
 		return false;
-	devid &= 0xffff;
 
 	iommu = rlookup_amd_iommu(dev);
 	if (!iommu)
@@ -379,7 +374,7 @@ static bool check_device(struct device *dev)
 
 	/* Out of our scope? */
 	pci_seg = iommu->pci_seg;
-	if (devid > pci_seg->last_bdf)
+	if ((devid & 0xffff) > pci_seg->last_bdf)
 		return false;
 
 	return true;
@@ -393,11 +388,10 @@ static int iommu_init_device(struct amd_iommu *iommu, struct device *dev)
 	if (dev_iommu_priv_get(dev))
 		return 0;
 
-	devid = get_device_sbdf_id(dev);
+	devid = get_device_id(dev);
 	if (devid < 0)
 		return devid;
 
-	devid &= 0xffff;
 	dev_data = find_dev_data(iommu, devid);
 	if (!dev_data)
 		return -ENOMEM;
@@ -426,11 +420,10 @@ static void iommu_ignore_device(struct amd_iommu *iommu, struct device *dev)
 	struct dev_table_entry *dev_table = get_dev_table(iommu);
 	int devid;
 
-	devid = get_device_sbdf_id(dev);
+	devid = (get_device_id(dev)) & 0xffff;
 	if (devid < 0)
 		return;
 
-	devid &= 0xffff;
 	pci_seg->rlookup_table[devid] = NULL;
 	memset(&dev_table[devid], 0, sizeof(struct dev_table_entry));
 
@@ -2259,11 +2252,9 @@ static void amd_iommu_get_resv_regions(struct device *dev,
 	struct amd_iommu_pci_seg *pci_seg;
 	int devid;
 
-	devid = get_device_sbdf_id(dev);
+	devid = get_device_id(dev);
 	if (devid < 0)
 		return;
-	devid &= 0xffff;
-
 	iommu = rlookup_amd_iommu(dev);
 	if (!iommu)
 		return;
@@ -3153,7 +3144,7 @@ static int get_devid(struct irq_alloc_info *info)
 		return get_hpet_devid(info->devid);
 	case X86_IRQ_ALLOC_TYPE_PCI_MSI:
 	case X86_IRQ_ALLOC_TYPE_PCI_MSIX:
-		return get_device_sbdf_id(msi_desc_to_dev(info->desc));
+		return get_device_id(msi_desc_to_dev(info->desc));
 	default:
 		WARN_ON_ONCE(1);
 		return -1;
