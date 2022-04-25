5ea76d6307ec79d7c5287a2e14e73ca2f02ecc7f
Brijesh Singh <brijesh.singh@amd.com>
2022-04-25 23:13:01 +0000

    x86/sev: Add RMP entry lookup helpers
    The snp_lookup_page_in_rmptable() can be used by the host to read the RMP
    entry for a given page. The RMP entry format is documented in AMD PPR, see
    https://bugzilla.kernel.org/attachment.cgi?id=296015.
    
    Co-developed-by: Ashish Kalra <ashish.kalra@amd.com>
    Signed-off-by: Ashish Kalra <ashish.kalra@amd.com>
    Signed-off-by: Brijesh Singh <brijesh.singh@amd.com>

---
+++ arch/x86/include/asm/sev-host.h
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * AMD SVM-SEV Host Support.
+ *
+ * Copyright (C) 2023 Advanced Micro Devices, Inc.
+ *
+ * Author: Ashish Kalra <ashish.kalra@amd.com>
+ *
+ */
+
+#include <asm/sev-common.h>
+
+#ifdef CONFIG_KVM_AMD_SEV
+int snp_lookup_rmpentry(u64 pfn, int *level);
+#else
+static inline int snp_lookup_rmpentry(u64 pfn, int *level) { return 0; }
+#endif
