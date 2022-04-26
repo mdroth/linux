7e0628c37c2fa31bfa66630a7075158024a57861
Tom Lendacky <thomas.lendacky@amd.com>
2022-04-26 19:21:40 +0000

    KVM: SEV: Support SEV-SNP AP Creation NAE event
    Add support for the SEV-SNP AP Creation NAE event. This allows SEV-SNP
    guests to alter the register state of the APs on their own. This allows
    the guest a way of simulating INIT-SIPI.
    
    A new event, KVM_REQ_UPDATE_PROTECTED_GUEST_STATE, is created and used
    so as to avoid updating the VMSA pointer while the vCPU is running.
    
    For CREATE
      The guest supplies the GPA of the VMSA to be used for the vCPU with
      the specified APIC ID. The GPA is saved in the svm struct of the
      target vCPU, the KVM_REQ_UPDATE_PROTECTED_GUEST_STATE event is added
      to the vCPU and then the vCPU is kicked.
    
    For CREATE_ON_INIT:
      The guest supplies the GPA of the VMSA to be used for the vCPU with
      the specified APIC ID the next time an INIT is performed. The GPA is
      saved in the svm struct of the target vCPU.
    
    For DESTROY:
      The guest indicates it wishes to stop the vCPU. The GPA is cleared
      from the svm struct, the KVM_REQ_UPDATE_PROTECTED_GUEST_STATE event is
      added to vCPU and then the vCPU is kicked.
    
    The KVM_REQ_UPDATE_PROTECTED_GUEST_STATE event handler will be invoked
    as a result of the event or as a result of an INIT. The handler sets the
    vCPU to the KVM_MP_STATE_UNINITIALIZED state, so that any errors will
    leave the vCPU as not runnable. Any previous VMSA pages that were
    installed as part of an SEV-SNP AP Creation NAE event are un-pinned. If
    a new VMSA is to be installed, the VMSA guest page is pinned and set as
    the VMSA in the vCPU VMCB and the vCPU state is set to
    KVM_MP_STATE_RUNNABLE. If a new VMSA is not to be installed, the VMSA is
    cleared in the vCPU VMCB and the vCPU state is left as
    KVM_MP_STATE_UNINITIALIZED to prevent it from being run.
    
    Signed-off-by: Tom Lendacky <thomas.lendacky@amd.com>
    Signed-off-by: Brijesh Singh <brijesh.singh@amd.com>
    Signed-off-by: Ashish Kalra <ashish.kalra@amd.com>
    [mdr: add handling for gmem]
    Signed-off-by: Michael Roth <michael.roth@amd.com>

--- arch/x86/kvm/svm/svm.h
+++ arch/x86/kvm/svm/svm.h
@@ -98,6 +98,7 @@ struct kvm_sev_info {
 	atomic_t migration_in_progress;
 	u64 snp_init_flags;
 	void *snp_context;      /* SNP guest context page */
+	u64 sev_features;	/* Features set at VMSA creation */
 };
 
 struct kvm_svm {
@@ -216,6 +217,10 @@ struct vcpu_sev_es_state {
 	bool ghcb_sa_free;
 
 	u64 ghcb_registered_gpa;
+
+	struct mutex snp_vmsa_mutex; /* Used to handle concurrent updates of VMSA. */
+	gpa_t snp_vmsa_gpa;
+	bool snp_ap_create;
 };
 
 struct vcpu_svm {
@@ -724,6 +729,7 @@ int sev_cpu_init(struct svm_cpu_data *sd);
 int sev_dev_get_attr(u64 attr, u64 *val);
 extern unsigned int max_sev_asid;
 void handle_rmp_page_fault(struct kvm_vcpu *vcpu, gpa_t gpa, u64 error_code);
+void sev_snp_init_protected_guest_state(struct kvm_vcpu *vcpu);
 #else
 static inline struct page *snp_safe_alloc_page(struct kvm_vcpu *vcpu) {
 	return alloc_page(GFP_KERNEL_ACCOUNT | __GFP_ZERO);
