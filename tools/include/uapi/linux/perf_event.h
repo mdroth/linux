01bceb7103bb63f1885a00abcac181c674797ade
Ravi Bangoria <ravi.bangoria@amd.com>
2022-06-16 17:06:30 +0530

    [WIP] perf tool: Sync include/uapi/linux/perf_event.h header
    Two new fields for mem_lvl_num has been introduced: PERF_MEM_LVLNUM_IO
    and PERF_MEM_LVLNUM_EXTN_MEM. Also, __PERF_SAMPLE_PHYS_ADDR_EARLY is
    introduce to be used internally by kernel. Kernel header already
    contains these definitions. Sync them into tools header as well.
    
    Signed-off-by: Ravi Bangoria <ravi.bangoria@amd.com>

--- tools/include/uapi/linux/perf_event.h
+++ tools/include/uapi/linux/perf_event.h
@@ -165,6 +165,7 @@ enum perf_event_sample_format {
 
 	PERF_SAMPLE_MAX = 1U << 25,		/* non-ABI */
 
+	__PERF_SAMPLE_PHYS_ADDR_EARLY		= 1ULL << 62, /* non-ABI; internal use */
 	__PERF_SAMPLE_CALLCHAIN_EARLY		= 1ULL << 63, /* non-ABI; internal use */
 };
 
@@ -1292,7 +1293,9 @@ union perf_mem_data_src {
 #define PERF_MEM_LVLNUM_L2	0x02 /* L2 */
 #define PERF_MEM_LVLNUM_L3	0x03 /* L3 */
 #define PERF_MEM_LVLNUM_L4	0x04 /* L4 */
-/* 5-0xa available */
+/* 5-0x8 available */
+#define PERF_MEM_LVLNUM_EXTN_MEM 0x9 /* Extension memory */
+#define PERF_MEM_LVLNUM_IO	0x0a /* I/O */
 #define PERF_MEM_LVLNUM_ANY_CACHE 0x0b /* Any cache */
 #define PERF_MEM_LVLNUM_LFB	0x0c /* LFB */
 #define PERF_MEM_LVLNUM_RAM	0x0d /* RAM */
