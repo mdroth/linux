56bb644cc47dbcbf7dc949be5931d62f27bd7c89
Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
2022-09-09 14:13:00 -0500

    (IN-REVIEW) KVM: x86: Un-inhibit APICv/AVIC when switching to x2APIC mode
    Currently, kvm_lapic_xapic_id_updated() checks if the xAPIC ID in
    the APIC_ID register differs from the vcpu ID. If so it inhibits
    APICv/AVIC.
    
    However, for vcpu 256 and above, the 8-bit xAPIC ID field in the APIC_ID
    register cannot support 32-bit x2APIC ID causing the kvm_xapic_id()
    to return invalid ID and fail the comparison with the vcpu ID.
    This causes APICv/AVIC inhibition for VM with more than 256 vcpus
    due to APIVC_INHIBIT_REASON_APIC_ID_MODIFIED.
    
    In this case, when guest switch to x2APIC mode, KVM needs to clear
    APIVC_INHIBIT_REASON_APIC_ID_MODIFIED.
    
    Fixes: 3743c2f02517 ("KVM: x86: inhibit APICv/AVIC on changes to APIC ID or APIC base")
    Cc: Maxim Levitsky <mlevitsk@redhat.com>
    Signed-off-by: Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>

--- arch/x86/kvm/lapic.c
+++ arch/x86/kvm/lapic.c
@@ -371,6 +371,7 @@ static inline void kvm_apic_set_x2apic_id(struct kvm_lapic *apic, u32 id)
 	kvm_lapic_set_reg(apic, APIC_ID, id);
 	kvm_lapic_set_reg(apic, APIC_LDR, ldr);
 	atomic_set_release(&apic->vcpu->kvm->arch.apic_map_dirty, DIRTY);
+	kvm_clear_apicv_inhibit(apic->vcpu->kvm, APICV_INHIBIT_REASON_APIC_ID_MODIFIED);
 }
 
 static inline int apic_lvt_enabled(struct kvm_lapic *apic, int lvt_type)
