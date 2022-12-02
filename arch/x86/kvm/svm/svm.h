7c988f9cf1075995ce3acf70279f7782408d9d37
Dionna Glaze <dionnaglaze@google.com>
2022-12-02 19:20:39 +0000

    x86/sev: Add KVM commands for instance certs
    The /dev/sev device has the ability to store host-wide certificates for
    the key used by the AMD-SP for SEV-SNP attestation report signing,
    but for hosts that want to specify additional certificates that are
    specific to the image launched in a VM, a different way is needed to
    communicate those certificates.
    
    This patch adds two new KVM ioctl commands: KVM_SEV_SNP_{GET,SET}_CERTS
    
    The certificates that are set with this command are expected to follow
    the same format as the host certificates, but that format is opaque
    to the kernel.
    
    The new behavior for custom certificates is that the extended guest
    request command will now return the overridden certificates if they
    were installed for the instance. The error condition for a too small
    data buffer is changed to return the overridden certificate data size
    if there is an overridden certificate set installed.
    
    Setting a 0 length certificate returns the system state to only return
    the host certificates on an extended guest request.
    
    We also increase the SEV_FW_BLOB_MAX_SIZE another 4K page to allow
    space for an extra certificate.
    
    Cc: Tom Lendacky <Thomas.Lendacky@amd.com>
    Cc: Paolo Bonzini <pbonzini@redhat.com>
    
    Signed-off-by: Dionna Glaze <dionnaglaze@google.com>
    Signed-off-by: Ashish Kalra <ashish.kalra@amd.com>

--- arch/x86/kvm/svm/svm.h
+++ arch/x86/kvm/svm/svm.h
@@ -94,6 +94,7 @@ struct kvm_sev_info {
 	u64 snp_init_flags;
 	void *snp_context;      /* SNP guest context page */
 	void *snp_certs_data;
+	unsigned int snp_certs_len; /* Size of instance override for certs */
 	struct mutex guest_req_lock;
 
 	u64 sev_features;	/* Features set at VMSA creation */
