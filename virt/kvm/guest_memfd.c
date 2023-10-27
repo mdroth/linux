d641eb88f61b57fe9a4522ea8eb1865fcb727d6e
Sean Christopherson <seanjc@google.com>
2023-10-27 11:21:59 -0700

    KVM: Add hugepage support for dedicated guest memory
    NOTE: upstream guest_memfd does not currently support 2MB pages, so the
          support added here is disabled by default. Use the
          kvm.gmem_2m_enabled=1 module parameter to switch it on.
    
    TODO: Allow hugepages for kvm_gmem_populate() (carefully to avoid
          EEXISTS errors when misaligned)
    
    Extended guest_memfd to allow backing guest memory with hugepages. This
    is done as a best-effort by default until a better-defined mechanism is
    put in place that can provide better control/assurances to userspace
    about hugepage allocations.
    
    When reporting the max order when KVM gets a pfn from guest_memfd, force
    order-0 pages if the hugepage is not fully contained by the memslot
    binding, e.g. if userspace requested hugepages but punches a hole in the
    memslot bindings in order to emulate x86's VGA hole.
    
    Link: https://lore.kernel.org/kvm/20231027182217.3615211-1-seanjc@google.com/T/#mccbd3e8bf9897f0ddbf864e6318d6f2f208b269c
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    Message-Id: <20231027182217.3615211-18-seanjc@google.com>
    [Allow even with CONFIG_TRANSPARENT_HUGEPAGE; dropped momentarily due to
     uneasiness about the API. - Paolo]
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    [mdr: based on discussion in the Link regarding original patch, make the
          following set of changes:
          - For now, don't introduce an opt-in flag to enable hugepage
            support. By default, just make a best-effort for PMD_ORDER
            allocations so that there are no false assurances to userspace
            that they'll get hugepages. Performance-wise, it's better at
            least than the current guarantee that they will get 4K pages
            every time. A more proper opt-in interface can then improve on
            things later.
          - Pass GFP_NOWARN to alloc_pages() so failures are not disruptive
            to normal operations
          - Drop size checks during creation time. Instead just avoid huge
            allocations if they extend beyond end of the memfd.
          - Drop hugepage-related unit tests since everything is now handled
            transparently to userspace anyway.
          - Update commit message accordingly.]
    Signed-off-by: Michael Roth <michael.roth@amd.com>

--- virt/kvm/guest_memfd.c
+++ virt/kvm/guest_memfd.c
@@ -49,14 +49,56 @@ static int kvm_gmem_prepare_folio(struct inode *inode, pgoff_t index, struct fol
 	return 0;
 }
 
-static struct folio *kvm_gmem_get_folio(struct inode *inode, pgoff_t index, bool prepare)
+static struct folio *kvm_gmem_get_huge_folio(struct inode *inode, pgoff_t index,
+					     unsigned int order)
 {
+	pgoff_t npages = 1UL << order;
+	pgoff_t huge_index = round_down(index, npages);
+	struct address_space *mapping  = inode->i_mapping;
+	gfp_t gfp = mapping_gfp_mask(mapping) | __GFP_NOWARN;
+	loff_t size = i_size_read(inode);
 	struct folio *folio;
 
-	/* TODO: Support huge pages. */
-	folio = filemap_grab_folio(inode->i_mapping, index);
-	if (IS_ERR(folio))
-		return folio;
+	/* Make sure hugepages would be fully-contained by inode */
+	if ((huge_index + npages) * PAGE_SIZE > size)
+		return NULL;
+
+	if (filemap_range_has_page(mapping, (loff_t)huge_index << PAGE_SHIFT,
+				   (loff_t)(huge_index + npages - 1) << PAGE_SHIFT))
+		return NULL;
+
+	folio = filemap_alloc_folio(gfp, order);
+	if (!folio)
+		return NULL;
+
+	if (filemap_add_folio(mapping, folio, huge_index, gfp)) {
+		folio_put(folio);
+		return NULL;
+	}
+
+	return folio;
+}
+
+static struct folio *kvm_gmem_get_folio(struct inode *inode, pgoff_t index, bool prepare)
+{
+	struct folio *folio = NULL;
+
+	/*
+	 * TODO: modify kvm_gmem_populate() so that order can be limited based on
+	 * alignment and other restrictions, otherwise __filemap_get_folio() may
+	 * get called with a smaller granularity and return EEXIST due to
+	 * FGP_CREAT_ONLY being set. For now just limit FGP_CREAT_ONLY cases to 4K.
+	 * (TODO: with FGP_CREAT_ONLY gone, it may be possible to do this
+	 * unconditionally again.
+	 */
+	if (prepare && gmem_2m_enabled)
+		folio = kvm_gmem_get_huge_folio(inode, index, PMD_ORDER);
+
+	if (!folio) {
+		folio = filemap_grab_folio(inode->i_mapping, index);
+		if (IS_ERR(folio))
+			return folio;
+	}
 
 	/*
 	 * Use the up-to-date flag to track whether or not the memory has been
@@ -420,6 +462,7 @@ static int __kvm_gmem_create(struct kvm *kvm, loff_t size, u64 flags)
 	inode->i_size = size;
 	mapping_set_gfp_mask(inode->i_mapping, GFP_HIGHUSER);
 	mapping_set_inaccessible(inode->i_mapping);
+	mapping_set_large_folios(inode->i_mapping);
 	/* Unmovable mappings are supposed to be marked unevictable as well. */
 	WARN_ON_ONCE(!mapping_unevictable(inode->i_mapping));
 
@@ -544,8 +587,8 @@ void kvm_gmem_unbind(struct kvm_memory_slot *slot)
 static int __kvm_gmem_get_pfn(struct file *file, struct kvm_memory_slot *slot,
 		       gfn_t gfn, kvm_pfn_t *pfn, int *max_order, bool prepare)
 {
-	pgoff_t index = gfn - slot->base_gfn + slot->gmem.pgoff;
 	struct kvm_gmem *gmem = file->private_data;
+	pgoff_t index, huge_index;
 	struct folio *folio;
 	struct page *page;
 	int r;
@@ -556,6 +599,7 @@ static int __kvm_gmem_get_pfn(struct file *file, struct kvm_memory_slot *slot,
 	}
 
 	gmem = file->private_data;
+	index = gfn - slot->base_gfn + slot->gmem.pgoff;
 	if (xa_load(&gmem->bindings, index) != slot) {
 		WARN_ON_ONCE(xa_load(&gmem->bindings, index));
 		return -EIO;
@@ -574,9 +618,24 @@ static int __kvm_gmem_get_pfn(struct file *file, struct kvm_memory_slot *slot,
 	page = folio_file_page(folio, index);
 
 	*pfn = page_to_pfn(page);
-	if (max_order)
-		*max_order = 0;
+	if (!max_order)
+		goto success;
 
+	*max_order = compound_order(compound_head(page));
+	if (!*max_order)
+		goto success;
+
+	/*
+	 * The folio can be mapped with a hugepage if and only if the folio is
+	 * fully contained by the range the memslot is bound to.  Note, the
+	 * caller is responsible for handling gfn alignment, this only deals
+	 * with the file binding.
+	 */
+	huge_index = ALIGN(index, 1ull << *max_order);
+	if (huge_index < ALIGN(slot->gmem.pgoff, 1ull << *max_order) ||
+	    huge_index + (1ull << *max_order) > slot->gmem.pgoff + slot->npages)
+		*max_order = 0;
+success:
 	r = 0;
 
 	folio_unlock(folio);
