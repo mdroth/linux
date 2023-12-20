ff87175395e26cbb8da694cb0ec66cb867f0a901
Brijesh Singh <brijesh.singh@amd.com>
2023-12-19 21:54:42 -0600

    x86/sev: Add the host SEV-SNP initialization support
    The memory integrity guarantees of SEV-SNP are enforced through a new
    structure called the Reverse Map Table (RMP). The RMP is a single data
    structure shared across the system that contains one entry for every 4K
    page of DRAM that may be used by SEV-SNP VMs. APM2 section 15.36 details
    a number of steps needed to detect/enable SEV-SNP and RMP table support
    on the host:
    
     - Detect SEV-SNP support based on CPUID bit
     - Initialize the RMP table memory reported by the RMP base/end MSR
       registers and configure IOMMU to be compatible with RMP access
       restrictions
     - Set the MtrrFixDramModEn bit in SYSCFG MSR
     - Set the SecureNestedPagingEn and VMPLEn bits in the SYSCFG MSR
     - Configure IOMMU
    
    RMP table entry format is non-architectural and it can vary by
    processor. It is defined by the PPR. Restrict SNP support to CPU
    models/families which are compatible with the current RMP table entry
    format to guard against any undefined behavior when running on other
    system types. Future models/support will handle this through an
    architectural mechanism to allow for broader compatibility.
    
    SNP host code depends on CONFIG_KVM_AMD_SEV config flag, which may be
    enabled even when CONFIG_AMD_MEM_ENCRYPT isn't set, so update the
    SNP-specific IOMMU helpers used here to rely on CONFIG_KVM_AMD_SEV
    instead of CONFIG_AMD_MEM_ENCRYPT.
    
    Signed-off-by: Brijesh Singh <brijesh.singh@amd.com>
    Co-developed-by: Ashish Kalra <ashish.kalra@amd.com>
    Signed-off-by: Ashish Kalra <ashish.kalra@amd.com>
    Co-developed-by: Tom Lendacky <thomas.lendacky@amd.com>
    Signed-off-by: Tom Lendacky <thomas.lendacky@amd.com>
    [mdr: rework commit message to be clearer about what patch does, squash
          in early_rmptable_check() handling from Tom]
    Signed-off-by: Michael Roth <michael.roth@amd.com>

--- arch/x86/kernel/cpu/amd.c
+++ arch/x86/kernel/cpu/amd.c
@@ -20,6 +20,7 @@
 #include <asm/delay.h>
 #include <asm/debugreg.h>
 #include <asm/resctrl.h>
+#include <asm/sev.h>
 
 #ifdef CONFIG_X86_64
 # include <asm/mmconfig.h>
@@ -574,6 +575,20 @@ static void bsp_init_amd(struct cpuinfo_x86 *c)
 		break;
 	}
 
+	if (cpu_has(c, X86_FEATURE_SEV_SNP)) {
+		/*
+		 * RMP table entry format is not architectural and it can vary by processor
+		 * and is defined by the per-processor PPR. Restrict SNP support on the
+		 * known CPU model and family for which the RMP table entry format is
+		 * currently defined for.
+		 */
+		if (!(c->x86 == 0x19 && c->x86_model <= 0xaf) &&
+		    !(c->x86 == 0x1a && c->x86_model <= 0xf))
+			setup_clear_cpu_cap(X86_FEATURE_SEV_SNP);
+		else if (!snp_probe_rmptable_info())
+			setup_clear_cpu_cap(X86_FEATURE_SEV_SNP);
+	}
+
 	return;
 
 warn:
@@ -628,7 +643,6 @@ clear_all:
 clear_sev:
 		setup_clear_cpu_cap(X86_FEATURE_SEV);
 		setup_clear_cpu_cap(X86_FEATURE_SEV_ES);
-		setup_clear_cpu_cap(X86_FEATURE_SEV_SNP);
 	}
 }
 
