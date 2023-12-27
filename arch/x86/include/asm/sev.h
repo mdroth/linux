7c089babb23342d8d60ed62db21939ba7229e05f
Michael Roth <michael.roth@amd.com>
2023-12-27 11:07:27 -0600

    KVM: SEV: Provide support for SNP_EXTENDED_GUEST_REQUEST NAE event
    Version 2 of GHCB specification added support for the SNP Extended Guest
    Request Message NAE event. This event serves a nearly identical purpose
    to the previously-added SNP_GUEST_REQUEST event, but allows for
    additional certificate data to be supplied via an additional
    guest-supplied buffer to be used mainly for verifying the signature of
    an attestation report as returned by firmware.
    
    This certificate data is supplied by userspace, so unlike with
    SNP_GUEST_REQUEST events, SNP_EXTENDED_GUEST_REQUEST events are first
    forwarded to userspace via a KVM_EXIT_VMGEXIT exit type, and then the
    firmware request is made only afterward.
    
    Implement handling for these events.
    
    Since there is a potential for race conditions where the
    userspace-supplied certificate data may be out-of-sync relative to the
    reported TCB that firmware will use when signing attestation reports,
    make use of the transaction/synchronization mechanisms added by the
    SNP_SET_CONFIG_{START,END} SEV device ioctls such that the guest will be
    told to retry the request when an update to reported TCB or
    userspace-supplied certificates may have occurred or is in progress
    while an extended guest request is being processed.
    
    Signed-off-by: Michael Roth <michael.roth@amd.com>

--- arch/x86/include/asm/sev.h
+++ arch/x86/include/asm/sev.h
@@ -272,6 +272,8 @@ int rmp_make_shared(u64 pfn, enum pg_level level);
 void snp_leak_pages(u64 pfn, unsigned int npages);
 u64 snp_config_transaction_start(void);
 u64 snp_config_transaction_end(void);
+u64 snp_config_transaction_get_id(void);
+bool snp_config_transaction_is_stale(u64 id);
 #else
 static inline bool snp_probe_rmptable_info(void) { return false; }
 static inline int snp_lookup_rmpentry(u64 pfn, bool *assigned, int *level) { return -ENODEV; }
@@ -286,6 +288,8 @@ static inline int rmp_make_shared(u64 pfn, enum pg_level level) { return -ENODEV
 static inline void snp_leak_pages(u64 pfn, unsigned int npages) {}
 static inline u64 snp_config_transaction_start(void) { return 0; }
 static inline u64 snp_config_transaction_end(void) { return 0; }
+static inline u64 snp_config_transaction_get_id(void) { return 0; }
+static inline bool snp_config_transaction_is_stale(u64 id) { return false; }
 #endif
 
 #endif
